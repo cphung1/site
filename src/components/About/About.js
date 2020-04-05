843e7ba65b99ef71c0be9ca4df5dcc8a752fa624
cphung1 <cphung97@gmail.com>
2020-04-05 01:17:43 -0700

    capitalized heading

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ export default function About(props) {
 
       <div className="about_me">
         <div className="about_text">
-        <h2>About me</h2>
+        <h2>About Me</h2>
         <p>Suspendisse ultricies ante sit amet lacus rhoncus, id vulputate nulla fermentum. Etiam vestibulum ipsum a accumsan sagittis. Praesent ullamcorper risus justo, id semper risus finibus ut. Sed vulputate augue turpis, quis posuere nibh dignissim eget. Proin a dolor ut quam tempor finibus id non purus. Duis at ipsum non dolor dapibus semper at sit amet magna. Nam iaculis tempus tellus at congue. Aliquam erat volutpat.</p>
 
         <h4>Technologies I've been working with: </h4>
